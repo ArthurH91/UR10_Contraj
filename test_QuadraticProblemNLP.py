0fbf8d58b10cf47ab0f624c19434f861f2d5e927
Arthur Haffemayer <arthur.haffemayer@laas.fr>
2023-04-11 09:38:41 +0200

    Commenting the tests + correcting test_get_difference_between_q_iter

--- test_QuadraticProblemNLP.py
+++ test_QuadraticProblemNLP.py
@@ -10,30 +10,43 @@ from RobotWrapper import RobotWrapper
 class TestQuadraticProblemNLP(unittest.TestCase):
 
     def test_get_q_iter_from_Q(self):
+        """Testing the function _get_iter_from_Q by comparing the first array of Q and the q_init, which should be the first array of Q 
+        """
         self.assertTrue(np.array_equal(QP._get_q_iter_from_Q(0), q_init),
                         msg="q_iter obtained thanks to the method QP._get_q_iter_from_Q differs from the real q_iter")
 
     def test_get_difference_between_q_iter(self):
-        self.assertTrue(np.array_equal(QP._get_q_iter_from_Q(1) - QP._get_q_iter_from_Q(
-            0), q_target - q_init), msg=" The difference between the q_iter and q_iter+1 is false")
+        """Testing the function _get_difference_between_q_iter by comparing its result and the difference between q_target and q_init that should be the first and second array of Q
+        """
+        self.assertTrue(np.array_equal(QP._get_difference_between_q_iter(0), q_target - q_init), msg=" The difference between the q_iter and q_iter+1 is false")
 
     def test_distance_endeff_target(self):
+        """Testing the function _distance_endeff_target by putting the robot at the configuration q_target whence the target was generated and testing whether the distance between the end-effector and the target is equal to 0
+        """
         self.assertAlmostEqual(np.linalg.norm(QP._distance_endeff_target(
             q_target)), 0, msg="Error while computing the distance between the end effector and the target")
         
     def test_compute_principal_residual(self):
+        """Testing the difference between the residual that was computed in QP and the handmade residual
+        """
         self.assertTrue(np.array_equal(principal_residual, residual_handmade), msg = "Error while computing principal residual")
 
     def test_compute_terminal_residual(self):
+        """Testing the difference between the terminal residual that was computed in QP and the handmade residual. As the robot is on the target, the terminal residual ought to be equal to 0
+        """
         self.assertEqual(np.linalg.norm(QP._terminal_residual), 0, msg = "Error while computing terminal residual")
 
     def test_compute_principal_cost(self):
+        """Testing the difference between the cost that was computed in QP and the handmade cost
+        """
         self.assertEqual(principal_cost, principal_cost_handmade, msg = "Error while computing the principal cost")
 
     def test_compute_cost(self):
         """As the robot is on the target, the terminal cost ought to be equal to 0, then the total cost is equal to the principal cost
         """
         self.assertEqual(principal_cost_handmade, cost, msg = "Error while computing the total cost")
+
+
 if __name__ == "__main__":
 
     # Setup of the environement
