cbf722321ac140a181ae9292bfda07fec6af07e6
Arthur Haffemayer <arthur.haffemayer@laas.fr>
2023-05-09 11:30:19 +0200

    changing the organization of the repo

--- src/contraj/src/contraj/demo_solving_problem_traj.py
+++ src/contraj/src/contraj/demo_solving_problem_traj.py
@@ -35,7 +35,7 @@ from wrapper_meshcat import MeshcatWrapper
 from problem_traj import QuadratricProblemNLP
 from solver_newton_mt import SolverNewtonMt
 from solver_casadi import CasadiSolver
-from utils import display_last_traj, generateReachableTarget
+from utils import display_last_traj, generate_reachable_target, numdiff
 
 # ### HYPERPARMS
 T = 6
@@ -50,14 +50,25 @@ INITIAL_CONFIG = np.array([0, -2.5, 2, -1.2, -1.7, 0])
 INITIAL_CONFIG = "random"
 
 SEED = abs(int(np.sin(time.time() % 6.28) * 1000))
-SEED = 573  # TRS does not perfectly converge, slight difference with IpOpt
-SEED = 1  # Perfect convergence to solution, immediate convergence of IpOpt (with WS)
+# SEED = 573  # TRS does not perfectly converge, slight difference with IpOpt
+# SEED = 1  # Perfect convergence to solution, immediate convergence of IpOpt (with WS)
 print(f"SEED = {SEED}")
 
 WITH_DISPLAY = True
 WITH_PLOT = True
-WITH_NUMDIFF_SOLVE = False
-WARMSTART_IPOPT_WITH_TRS = False
+WITH_NUMDIFF_SOLVE = True
+WARMSTART_IPOPT_WITH_TRS = True
+WITH_CASADI = True
+
+
+### HELPERS (Finite difference comutation of the gradient and the hessian)
+if WITH_NUMDIFF_SOLVE:
+
+    def grad_numdiff(Q: np.ndarray):
+        return numdiff(QP.cost, Q)
+
+    def hess_numdiff(Q: np.ndarray):
+        return numdiff(grad_numdiff, Q)
 
 
 if __name__ == "__main__":
@@ -73,7 +84,7 @@ if __name__ == "__main__":
     if INITIAL_CONFIG == "random":
         INITIAL_CONFIG = pin.randomConfiguration(rmodel)
     if TARGET == "random":
-        TARGET = generateReachableTarget(rmodel, rdata)
+        TARGET = generate_reachable_target(rmodel, rdata)
 
     # Creating the QP
     QP = QuadratricProblemNLP(
@@ -119,7 +130,7 @@ if __name__ == "__main__":
 
         # Trust region solver with finite difference
         trust_region_solver_nd = SolverNewtonMt(
-            QP.cost, QP._grad_numdiff, QP._hess_numdiff, max_iter=100, callback=None
+            QP.cost, grad_numdiff, hess_numdiff, max_iter=100, callback=None
         )
         res = trust_region_solver_nd(Q0)
         list_fval_mt_nd, list_gradfkval_mt_nd, list_alphak_mt_nd, list_reguk_nd = (
@@ -131,22 +142,23 @@ if __name__ == "__main__":
         Q_nd = trust_region_solver_nd._xval_k
 
     # Casadi+IpOpt solver
-    casadiSolver = CasadiSolver(QP)
-    Q_casadi, residuals_casadi = casadiSolver.solve(
-        Q_trs if WARMSTART_IPOPT_WITH_TRS else None
-    )
-    J_casadi = casadiSolver.evalJacobian(Q_casadi)
+    if WITH_CASADI:
+        casadiSolver = CasadiSolver(QP)
+        Q_casadi, residuals_casadi = casadiSolver.solve(
+            Q_trs if WARMSTART_IPOPT_WITH_TRS else None
+        )
+        J_casadi = casadiSolver.evalJacobian(Q_casadi)
 
-    # ### NUMDIFF unittest
-    Qr = np.random.rand((T + 1) * rmodel.nq) * 6 - 3
-    gnd = QP._grad_numdiff(Qr)
-    Jr = casadiSolver.evalJacobian(Qr)
-    rr = casadiSolver.evalResiduals(Qr)
-    gcas = Jr.T @ rr
-    galg = QP.grad(Qr)
-    assert norm(rr - QP._residual, np.inf) < 1e-9
-    assert norm(gcas - galg, np.inf) < 1e-9
-    assert norm(gnd - galg, np.inf) < 1e-3
+        # ### NUMDIFF unittest
+        Qr = np.random.rand((T + 1) * rmodel.nq) * 6 - 3
+        gnd = grad_numdiff(Qr)
+        Jr = casadiSolver.evalJacobian(Qr)
+        rr = casadiSolver.evalResiduals(Qr)
+        gcas = Jr.T @ rr
+        galg = QP.grad(Qr)
+        assert norm(rr - QP._residual, np.inf) < 1e-9
+        assert norm(gcas - galg, np.inf) < 1e-9
+        assert norm(gnd - galg, np.inf) < 1e-3
 
     if WITH_DISPLAY:
         print(
@@ -196,19 +208,27 @@ if __name__ == "__main__":
         )
         plt.show()
 
-    np.set_printoptions(precision=3, linewidth=600, suppress=True)
-    print(
-        "Optimal trajectory: \n\t",
-        "\n\t ".join([repr(q) for q in np.split(Q_casadi, T + 1)]),
-    )
-    pin.framesForwardKinematics(rmodel, rdata, Q_casadi[-rmodel.nq :])
-    print("Terminal position:", rdata.oMf[QP._EndeffID].translation, " vs ", QP._target)
-    print(f"Distance between IpOpt and TRS solvers {norm(Q_casadi-Q_trs,np.inf)} ")
-    if WITH_NUMDIFF_SOLVE:
-        print(f"Distance between IpOpt and ND solvers {norm(Q_casadi-Q_nd,np.inf)} ")
-        print(f"Distance between ND and TRS solvers {norm(Q_nd-Q_trs,np.inf)} ")
+    if WITH_CASADI:
+        np.set_printoptions(precision=3, linewidth=600, suppress=True)
+        print(
+            "Optimal trajectory: \n\t",
+            "\n\t ".join([repr(q) for q in np.split(Q_casadi, T + 1)]),
+        )
+        pin.framesForwardKinematics(rmodel, rdata, Q_casadi[-rmodel.nq :])
+        print(
+            "Terminal position:",
+            rdata.oMf[QP._EndeffID].translation,
+            " vs ",
+            QP._target,
+        )
+        print(f"Distance between IpOpt and TRS solvers {norm(Q_casadi-Q_trs,np.inf)} ")
+        if WITH_NUMDIFF_SOLVE:
+            print(
+                f"Distance between IpOpt and ND solvers {norm(Q_casadi-Q_nd,np.inf)} "
+            )
+            print(f"Distance between ND and TRS solvers {norm(Q_nd-Q_trs,np.inf)} ")
 
-    assert np.allclose(Q_casadi, Q_trs, atol=1e-7, rtol=1e-3)
-    if WITH_NUMDIFF_SOLVE:
-        assert np.allclose(Q_casadi, Q_nd, atol=1e-3, rtol=10)
-        assert np.allclose(Q_trs, Q_nd, atol=1e-3, rtol=10)
+        assert np.allclose(Q_casadi, Q_trs, atol=1e-7, rtol=1e-3)
+        if WITH_NUMDIFF_SOLVE:
+            assert np.allclose(Q_casadi, Q_nd, atol=1e-3, rtol=10)
+            assert np.allclose(Q_trs, Q_nd, atol=1e-3, rtol=10)
